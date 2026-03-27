86644ed968aff5c6910a32aedf0f5905d23527b1
marco <marcogirarditesero@gmail.com>
2026-03-27 23:34:41 +0100

    Fatto?

--- SimpleCtlModelChecker.CSharp/SimpleCtlModelChecker.CSharp/Visitors/DenotationCtlFormulaVisitor.cs
+++ SimpleCtlModelChecker.CSharp/SimpleCtlModelChecker.CSharp/Visitors/DenotationCtlFormulaVisitor.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using SimpleCtlModelChecker.CSharp.Exceptions;
 using SimpleCtlModelChecker.CSharp.Extensions;
 using SimpleCtlModelChecker.CSharp.Formulae;
 using SimpleCtlModelChecker.CSharp.Model;
@@ -8,15 +9,15 @@ namespace SimpleCtlModelChecker.CSharp.Visitors;
 
 public class DenotationCtlFormulaVisitor(KripkeModel model) : CtlFormulaVisitor<ImmutableHashSet<string>>
 {
-    private readonly KripkeModel _model = model;
-    private ImmutableHashSet<string> StateSet => _model.States.Select(s => s.Name).ToImmutableHashSet();
+    private const int MaxFixpointIterations = 10000;
+    private ImmutableHashSet<string> StateSet => model.States.Select(s => s.Name).ToImmutableHashSet();
 
 
     protected override ImmutableHashSet<string> VisitBottom(BottomCtlFormula bottomCtlFormula)
         => [];
 
     protected override ImmutableHashSet<string> VisitAtomic(AtomicCtlFormula atomicCtlFormula)
-        => _model
+        => model
             .States
             .Where(s => s.Atoms.Contains(atomicCtlFormula.Atom))
             .Select(s => s.Name)
@@ -60,35 +61,115 @@ public class DenotationCtlFormulaVisitor(KripkeModel model) : CtlFormulaVisitor<
 
 #region UNARY TEMPORAL OPERATORS
 
-    private ImmutableHashSet<string> VisitAG(ImmutableHashSet<string> operand)
+    private ImmutableHashSet<string> VisitAG(ImmutableHashSet<string> operandDenotation)
     {
-        throw new NotImplementedException();
+        // AGp <-> p /\ AXAGp 
+        // we want to compute a maximum fixpoint of p /\ AXp basically
+        var count = 0;
+        var setToIterateOn = new HashSet<string>(operandDenotation); // we can keep this mutable to save allocations, if we care of that
+        var elementsCount = setToIterateOn.Count;
+
+        while (count < MaxFixpointIterations)
+        {
+            setToIterateOn.IntersectWith(VisitAX(setToIterateOn.ToImmutableHashSet())); // so much for saving the allocations lmao
+
+            if (setToIterateOn.Count == elementsCount)
+            {
+                // fixpoint reached!
+                return setToIterateOn.ToImmutableHashSet();
+            }
+
+            elementsCount = setToIterateOn.Count;
+            count++;
+        }
+
+        throw new CtlModelCheckerException("Max number of fixpoint iterations exceeded");
     }
 
-    private ImmutableHashSet<string> VisitEG(ImmutableHashSet<string> operand)
+    private ImmutableHashSet<string> VisitEG(ImmutableHashSet<string> operandDenotation)
     {
-        throw new NotImplementedException();
+        // EGp <-> p /\ EXEGp 
+        // we want to compute a maximum fixpoint of p /\ EXp basically
+        var count = 0;
+        var setToIterateOn = new HashSet<string>(operandDenotation); // we can keep this mutable to save allocations, if we care of that
+        var elementsCount = setToIterateOn.Count;
+
+        while (count < MaxFixpointIterations)
+        {
+            setToIterateOn.IntersectWith(VisitEX(setToIterateOn.ToImmutableHashSet())); // so much for saving the allocations lmao
+
+            if (setToIterateOn.Count == elementsCount)
+            {
+                // fixpoint reached!
+                return setToIterateOn.ToImmutableHashSet();
+            }
+
+            elementsCount = setToIterateOn.Count;
+            count++;
+        }
+
+        throw new CtlModelCheckerException("Max number of fixpoint iterations exceeded");
     }
 
-    private ImmutableHashSet<string> VisitAF(ImmutableHashSet<string> operand)
+    private ImmutableHashSet<string> VisitAF(ImmutableHashSet<string> operandDenotation)
     {
-        throw new NotImplementedException();
+        // AFp <-> p \/ AXAFp 
+        // we want to compute a minimum fixpoint of p \/ AXp basically
+        var count = 0;
+        var setToIterateOn = new HashSet<string>(operandDenotation); // we can keep this mutable to save allocations, if we care of that
+        var elementsCount = setToIterateOn.Count;
+
+        while (count < MaxFixpointIterations)
+        {
+            setToIterateOn.UnionWith(VisitAX(setToIterateOn.ToImmutableHashSet())); // so much for saving the allocations lmao
+
+            if (setToIterateOn.Count == elementsCount)
+            {
+                // fixpoint reached!
+                return setToIterateOn.ToImmutableHashSet();
+            }
+
+            elementsCount = setToIterateOn.Count;
+            count++;
+        }
+
+        throw new CtlModelCheckerException("Max number of fixpoint iterations exceeded");
     }
 
-    private ImmutableHashSet<string> VisitEF(ImmutableHashSet<string> operand)
+    private ImmutableHashSet<string> VisitEF(ImmutableHashSet<string> operandDenotation)
     {
-        throw new NotImplementedException();
+        // EFp <-> p \/ EXEFp 
+        // we want to compute a minimum fixpoint of p \/ EXp basically
+        var count = 0;
+        var setToIterateOn = new HashSet<string>(operandDenotation); // we can keep this mutable to save allocations, if we care of that
+        var elementsCount = setToIterateOn.Count;
+
+        while (count < MaxFixpointIterations)
+        {
+            setToIterateOn.UnionWith(VisitEX(setToIterateOn.ToImmutableHashSet())); // so much for saving the allocations lmao
+
+            if (setToIterateOn.Count == elementsCount)
+            {
+                // fixpoint reached!
+                return setToIterateOn.ToImmutableHashSet();
+            }
+
+            elementsCount = setToIterateOn.Count;
+            count++;
+        }
+
+        throw new CtlModelCheckerException("Max number of fixpoint iterations exceeded");
     }
 
-    private ImmutableHashSet<string> VisitAX(ImmutableHashSet<string> operand)
-    {
-        throw new NotImplementedException();
-    }
+    private ImmutableHashSet<string> VisitAX(ImmutableHashSet<string> operandDenotation)
+        => model.States.Where(s => s.Transitions.All(operandDenotation.Contains))
+            .Select(s => s.Name)
+            .ToImmutableHashSet();
 
-    private ImmutableHashSet<string> VisitEX(ImmutableHashSet<string> operand)
-    {
-        throw new NotImplementedException();
-    }
+    private ImmutableHashSet<string> VisitEX(ImmutableHashSet<string> operandDenotation)
+        => model.States.Where(s => s.Transitions.Any(operandDenotation.Contains))
+            .Select(s => s.Name)
+            .ToImmutableHashSet();
 
 #endregion
 
@@ -96,12 +177,52 @@ public class DenotationCtlFormulaVisitor(KripkeModel model) : CtlFormulaVisitor<
 
     private ImmutableHashSet<string> VisitAU(ImmutableHashSet<string> leftDenotation, ImmutableHashSet<string> rightDenotation)
     {
-        throw new NotImplementedException();
+        // A[pUq] <-> q \/ (p /\ AXA[pUq]) 
+        // we want to compute a minimum fixpoint of q \/ (p /\ AXq) basically
+        var count = 0;
+        var setToIterateOn = new HashSet<string>(rightDenotation); // we can keep this mutable to save allocations, if we care of that
+        var elementsCount = setToIterateOn.Count;
+
+        while (count < MaxFixpointIterations)
+        {
+            setToIterateOn.UnionWith(leftDenotation.Intersect(VisitAX(setToIterateOn.ToImmutableHashSet()))); // so much for saving the allocations lmao
+
+            if (setToIterateOn.Count == elementsCount)
+            {
+                // fixpoint reached!
+                return setToIterateOn.ToImmutableHashSet();
+            }
+
+            elementsCount = setToIterateOn.Count;
+            count++;
+        }
+
+        throw new CtlModelCheckerException("Max number of fixpoint iterations exceeded");
     }
 
     private ImmutableHashSet<string> VisitEU(ImmutableHashSet<string> leftDenotation, ImmutableHashSet<string> rightDenotation)
     {
-        throw new NotImplementedException();
+        // E[pUq] <-> q \/ (p /\ EXE[pUq]) 
+        // we want to compute a minimum fixpoint of q \/ (p /\ EXq) basically
+        var count = 0;
+        var setToIterateOn = new HashSet<string>(rightDenotation); // we can keep this mutable to save allocations, if we care of that
+        var elementsCount = setToIterateOn.Count;
+
+        while (count < MaxFixpointIterations)
+        {
+            setToIterateOn.UnionWith(leftDenotation.Intersect(VisitEX(setToIterateOn.ToImmutableHashSet()))); // so much for saving the allocations lmao
+
+            if (setToIterateOn.Count == elementsCount)
+            {
+                // fixpoint reached!
+                return setToIterateOn.ToImmutableHashSet();
+            }
+
+            elementsCount = setToIterateOn.Count;
+            count++;
+        }
+
+        throw new CtlModelCheckerException("Max number of fixpoint iterations exceeded");
     }
 
 #endregion
